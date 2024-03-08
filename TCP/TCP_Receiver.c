16dc942ac377ee2c6b71899685604a2013b7cbd7
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-03-08 17:01:05 +0200

    Add files via upload

--- TCP/TCP_Receiver.c
+++ TCP/TCP_Receiver.c
@@ -1,51 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <time.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 2*1024*1024
 
-float receive_file(int server_sock) {
-    FILE *file;
-    char buffer[BUFFER_SIZE];
-    time_t start, end;
-    float time_taken;
-    int bytesReceived;
-
-    file = fopen("received_file.txt", "w");
-    if (file == NULL) {
-        perror("Error creating file");
+int main(int argc, char *argv[]) {
+    printf("Starting Receiver...\n");
+    if (argc < 5) {
+        printf("Invalid arguments.\nUsage: %s -p <port> -algo <algorithm>\n",argv[0]);
         return 0;
     }
 
-    start = clock();
-
-    while ((bytesReceived = recv(server_sock, buffer, BUFFER_SIZE, 0)) > 0) {
-        fwrite(buffer, 1, bytesReceived, file);
-        if (bytesReceived < BUFFER_SIZE)
-            break;
-    }
-    fclose(file);
-
-    end = clock();
-    time_taken = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
-
-    printf("File transfer completed.\n");
-    return time_taken;
-}
-
-int main() {
-    printf("Starting Receiver...\n");
-
-    int server_sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
     socklen_t len;
     char buffer[BUFFER_SIZE];
+    int port = atoi(argv[2]);
+    char *algo;
+    if(strcmp(argv[4],"reno") == 0||strcmp(argv[4],"cubic") == 0){
+        algo = argv[4];
+    } else{ return 1; }
+
     float *timesArr = NULL;
     timesArr = (float*)malloc(sizeof(float));
     int timesArr_size = 0;
@@ -54,56 +32,115 @@ int main() {
     int mbArr_size = 0;
     int countTimes = 0;
 
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
+    struct sockaddr_in server_addr, client_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(port);
 
     if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         return 1;
     }
-    if (listen(server_sock, 5) < 0) {
+    if (listen(server_sock, 1) < 0) {
         perror("Listen failed");
         return 1;
     }
     printf("Waiting for TCP connection...\n");
-
     len = sizeof(client_addr);
-    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &len);
+    int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &len);
     if (client_sock < 0) {
         perror("Accept failed");
         return 1;
     } 
-    printf("Sender connected, beginning to receive file...\n");
-    while (1) {
-        int bytesReceived = recv(client_sock, buffer, BUFFER_SIZE, 0);
-        if (bytesReceived < 0) {
-            perror("Receive error");
-            break;
-        } else if (bytesReceived == 0) {
-            printf("Sender closed the connection.\n");
-            break;
-        } else {
-            buffer[bytesReceived] = '\0';
-            if (strcmp(buffer, "exit") == 0) {
-                printf("Sender sent exit message.\n");
-                break;
-            } else {
-                timesArr = (float*)realloc(timesArr,(timesArr_size+1)*sizeof(float));
-                timesArr[timesArr_size++] = receive_file(client_sock);
-                mbArr = (double*)realloc(mbArr,(mbArr_size+1)*sizeof(double));
-                mbArr[mbArr_size++] = (bytesReceived/1024.0)/(timesArr[timesArr_size-1]/1000);
-                countTimes++;
-            }
-        }
+    printf("Sender connected...\n");
+    
+    if (setsockopt(client_sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0) {
+        perror("setsockopt");
+        return 1;
     }
+    printf("Set algo to %s, beginning to receive file...\n", algo);
+
+    time_t start, end;
+    float time_taken;
+
+    // while (1) {
+    //     size_t total_bytes = 0;  // the total bytes received so far
+    //     start = clock();  // start measuring the time
+    //     ssize_t bytes_received = recv(client_sock, buffer, sizeof(buffer), 0);
+    //     total_bytes += bytes_received;
+    //     if (bytes_received == -1) {
+    //         perror("recv");
+    //         return 1;
+    //     }
+    //     if (strncmp(buffer, "exit", 4) == 0) {
+    //         printf("exit message received\n");
+    //         break;
+    //     }
+    //     while (total_bytes < sizeof(buffer)) {
+    //         bytes_received = recv(client_sock, buffer + total_bytes, sizeof(buffer) - total_bytes, 0);
+    //         if (bytes_received == -1) {
+    //             perror("recv");
+    //             return 1;
+    //         }
+    //         if (bytes_received == 0) {
+    //             printf("sender closed the connection\n");
+    //             break;
+    //         }
+    //         total_bytes += bytes_received;
+    //     }
+    //     end = clock();
+    //     printf("File transfer completed.\n");
+    //     time_taken = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
+    //     timesArr = (float*)realloc(timesArr,(timesArr_size+1)*sizeof(float));
+    //     timesArr[timesArr_size++] = time_taken;
+    //     mbArr = (double*)realloc(mbArr,(mbArr_size+1)*sizeof(double));
+    //     mbArr[mbArr_size++] = (bytes_received/1024.0)/(time_taken/1000);
+    //     countTimes++;
+    // }
+    while(1){
+        size_t total_bytes = 0;  // the total bytes received so far
+        start = clock();  // start measuring the time
+        ssize_t bytes_received = recv(client_sock, buffer, sizeof(buffer), 0);
+        total_bytes += bytes_received;
+
+        if (bytes_received == -1) {  // check for errors
+            perror("recv");
+            return 1;
+        }
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("exit message received\n");
+            break;
+        }
+        // keep receiving until the file is received
+        while (total_bytes < sizeof(buffer)) {
+            bytes_received = recv(client_sock, buffer + total_bytes, sizeof(buffer) - total_bytes, 0);
+            if (bytes_received == -1) {  // check for errors
+                perror("recv");
+                return 1;
+            }
+            if (bytes_received == 0) {
+                printf("sender closed the connection\n");
+                break;
+            }
+            total_bytes += bytes_received;
+        }
+        end = clock();
+        printf("File transfer completed.\n");
+        time_taken = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
+        timesArr = (float*)realloc(timesArr,(timesArr_size+1)*sizeof(float));
+        timesArr[timesArr_size++] = time_taken;
+        mbArr = (double*)realloc(mbArr,(mbArr_size+1)*sizeof(double));
+        mbArr[mbArr_size++] = (bytes_received/1024.0)/(time_taken/1000);
+        countTimes++;
+    }
+    
     printf("----------------------------------");
     printf("STATISTICS :\n");
     float totalTime=0;
@@ -113,8 +150,8 @@ int main() {
         totalTime += timesArr[i];
         totalMS += mbArr[i];
     }
-    printf("\nAverage time: %fms.\n", totalTime/countTimes);
-    printf("\nAverage bandwidth:: %fmb.\n", totalMS/countTimes);
+    printf("\nAverage time: %fms.\n", (totalTime/countTimes));
+    printf("\nAverage bandwidth:: %fmb.\n", (totalMS/countTimes));
     printf("----------------------------------");
     printf("Receiver end.");
     close(client_sock);
--- TCP/TCP_Sender.c
+++ TCP/TCP_Sender.c
@@ -1,57 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <time.h>
 
-#define PORT 8080
 #define BUFFER_SIZE 1024
+#define DATA_SIZE 2*1024*1024
 
-int main() {
-    int sock;
+char *util_generate_random_data(unsigned int size) {
+    char *buffer = NULL;
+    // Argument check.
+    if (size == 0)
+    return NULL;
+    buffer = (char *)calloc(size, sizeof(char));
+    // Error checking.
+    if (buffer == NULL)
+    return NULL;
+    // Randomize the seed of the random number generator.
+    srand(time(NULL));
+    for (unsigned int i = 0; i < size; i++)
+    *(buffer + i) = ((unsigned int)rand() % 256);
+    return buffer;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 7) {
+        printf("Send: %s -ip <IP> -p <port> -algo <algorithm>\n", argv[0]);
+        return 1;
+    }
     char buffer[BUFFER_SIZE];
-    struct sockaddr_in addr;
-    FILE *file;
-    char *filename = "file.txt";
+    char *ip = argv[2];
+    int port = atoi(argv[4]);
+    char *algo;
+    if(strcmp(argv[6],"reno") == 0||strcmp(argv[6],"cubic") == 0){
+        algo = argv[6];
+    } else{ return 1; }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    char* data = util_generate_random_data(DATA_SIZE);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
+    struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
 
     if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("Connection failed");
         return 1;
     } else{ printf("connected to server\n"); }
 
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0) {
+        perror("setsockopt");
+        return 1;
+    }
+
     while (1) {
-        file = fopen(filename, "w");
-        if (file == NULL) {
-            perror("Error opening file");
-            return 1;
+        size_t bytes_sent = 0;
+        while (bytes_sent < DATA_SIZE) {
+            size_t bytes_to_send = BUFFER_SIZE < DATA_SIZE - bytes_sent ? BUFFER_SIZE : DATA_SIZE - bytes_sent;
+            memcpy(buffer, data + bytes_sent, bytes_to_send);
+            ssize_t sent = send(sock, buffer, bytes_to_send, 0);
+            if (sent < 0) {
+                perror("ERROR writing to socket");
+            }
+            bytes_sent += sent;
         }
-        fprintf(file, "1\n19\nGrace Hopper coined \"bug\" for computer glitches. In 1947, she found a moth causing issues in Harvard's Mark II.\n3\n4\n13\n9\n5\n3\n6\n5\n5\n2\n5\ncomputer\n3\n10\n3\n0");
-        fclose(file);
-
-        file = fopen(filename, "r");
-        if (file == NULL) {
-            perror("Error opening file");
-            return 1;
-        }
-
-        while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-            send(sock, buffer, strlen(buffer), 0);
-        }
-        fclose(file);
-
         printf("File sent successfully.\n");
         
         char choice;
