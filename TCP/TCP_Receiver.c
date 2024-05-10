ea2a52b12b142c9c238db20f31dd3082e5f25296
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-05-11 00:53:13 +0300

    Add files via upload

--- TCP/TCP_Receiver.c
+++ TCP/TCP_Receiver.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[]) {
     printf("Starting Receiver...\n");
-    if (argc < 5) {
+    if (argc < 5) {                          // if there are inputs missing return error
         printf("Invalid arguments.\nUsage: %s -p <port> -algo <algorithm>\n",argv[0]);
         return 0;
     }
@@ -20,48 +20,48 @@ int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
     int port = atoi(argv[2]);
     char *algo;
-    if(strcmp(argv[4],"reno") == 0||strcmp(argv[4],"cubic") == 0){
+    if(strcmp(argv[4],"reno") == 0||strcmp(argv[4],"cubic") == 0){  // if not reno/cubic return error
         algo = argv[4];
     } else{ return 1; }
-
-    float *timesArr = NULL;
+ 
+    float *timesArr = NULL;                          // list of times for statistics
     timesArr = (float*)malloc(sizeof(float));
     int timesArr_size = 0;
-    double *mbArr = NULL;
+    double *mbArr = NULL;                            // list of mb/s for statistics
     mbArr = (double*)malloc(sizeof(double));
     int mbArr_size = 0;
     int countTimes = 0;
 
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);    // create IPv4 socket 
     if (server_sock < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in server_addr, client_addr;       // add address family for IPv4 socket
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) { //bind socket to address
         perror("Bind failed");
         return 1;
     }
-    if (listen(server_sock, 1) < 0) {
+    if (listen(server_sock, 1) < 0) {  // allow listening to 1 client
         perror("Listen failed");
         return 1;
     }
     printf("Waiting for TCP connection...\n");
     len = sizeof(client_addr);
-    int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &len);
+    int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &len); // accept clients socket
     if (client_sock < 0) {
         perror("Accept failed");
         return 1;
     } 
     printf("Sender connected...\n");
     
-    if (setsockopt(client_sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0) {
+    if (setsockopt(client_sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0) {  // set algo reno/cubic
         perror("setsockopt");
         return 1;
     }
@@ -72,22 +72,22 @@ int main(int argc, char *argv[]) {
 
     while(1){
         size_t total_bytes = 0;
-        start = clock();  // start measuring the time
+        start = clock();  // start timer
         ssize_t bytes_received = recv(client_sock, buffer, sizeof(buffer), 0);
         total_bytes += bytes_received;
 
-        if (bytes_received == -1) {  // check for errors
+        if (bytes_received == -1) {
             perror("recv");
             return 1;
         }
-        if (strncmp(buffer, "exit", 4) == 0) {
+        if (strncmp(buffer, "exit", 4) == 0) {  // if exit message then leave
             printf("exit message received\n");
             break;
         }
-        // keep receiving until the file is received
-        while (total_bytes < sizeof(buffer)) {
+
+        while (total_bytes < sizeof(buffer)) {  // receive data piece by piece
             bytes_received = recv(client_sock, buffer + total_bytes, sizeof(buffer) - total_bytes, 0);
-            if (bytes_received == -1) {  // check for errors
+            if (bytes_received == -1) { 
                 perror("recv");
                 return 1;
             }
@@ -97,30 +97,30 @@ int main(int argc, char *argv[]) {
             }
             total_bytes += bytes_received;
         }
-        end = clock();
+        end = clock(); // close timer
         printf("File transfer completed.\n");
-        time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
+        time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;  // time in seconds
         timesArr = (float*)realloc(timesArr,(timesArr_size+1)*sizeof(float));
-        timesArr[timesArr_size++] = time_taken * 1000;
+        timesArr[timesArr_size++] = time_taken * 1000;      // add to list of times and time in ms
         mbArr = (double*)realloc(mbArr,(mbArr_size+1)*sizeof(double));
-        mbArr[mbArr_size++] = 2/(time_taken);
-        countTimes++;
+        mbArr[mbArr_size++] = 2/(time_taken);               // we always send 2mb so 2mb/seconds taken to send
+        countTimes++; // count number of sends
     }
     
-    printf("----------------------------------");
+    printf("----------------------------------\n");
     printf("STATISTICS :\n");
     float totalTime=0;
     float totalMS=0;
     for(int i=0; i<countTimes; i++){
-        printf("Run #%d Data: Time=%fms; Speed=%fMB/s\n", i+1, timesArr[i], mbArr[i]);
-        totalTime += timesArr[i];
-        totalMS += mbArr[i];
+        printf("Run #%d Data: Time=%fms; Speed=%fMB/s\n", i+1, timesArr[i], mbArr[i]); //print send stats
+        totalTime += timesArr[i];  // sum of ms
+        totalMS += mbArr[i];  // sum of mb/s
     }
-    printf("\nAverage time: %fms.\n", (totalTime/countTimes));
-    printf("\nAverage bandwidth:: %fmb.\n", (totalMS/countTimes));
-    printf("----------------------------------");
+    printf("\nAverage time: %fms.\n", (totalTime/countTimes));    // average ms for send
+    printf("\nAverage bandwidth:: %fmb.\n", (totalMS/countTimes)); // average mb/s for send
+    printf("----------------------------------\n");
     printf("Receiver end.");
-    close(client_sock);
+    close(client_sock); // close connection
     close(server_sock);
 
     return 0;
--- TCP/TCP_Sender.c
+++ TCP/TCP_Sender.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 #include <time.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 65536*2
 #define DATA_SIZE 2*1024*1024
 
 char *util_generate_random_data(unsigned int size) {
@@ -27,66 +27,63 @@ char *util_generate_random_data(unsigned int size) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 7) {
+    if (argc < 7) {        // if there are inputs missing return error
         printf("Send: %s -ip <IP> -p <port> -algo <algorithm>\n", argv[0]);
         return 1;
     }
-    char buffer[BUFFER_SIZE];
     char *ip = argv[2];
     int port = atoi(argv[4]);
     char *algo;
-    if(strcmp(argv[6],"reno") == 0||strcmp(argv[6],"cubic") == 0){
+    if(strcmp(argv[6],"reno") == 0||strcmp(argv[6],"cubic") == 0){  // if not reno/cubic return error
         algo = argv[6];
     } else{ return 1; }
 
-    char* data = util_generate_random_data(DATA_SIZE);
+    char* data = util_generate_random_data(DATA_SIZE);   // generate random data
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);    // create IPv4 socket 
     if (sock < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
-    struct sockaddr_in addr;
+    struct sockaddr_in addr;                  // add address family for IPv4 socket
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = inet_addr(ip);
 
-    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {   // connect to server
         perror("Connection failed");
         return 1;
     } else{ printf("connected to server\n"); }
 
-    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0) {
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0) {  // set algo reno/cubic
         perror("setsockopt");
         return 1;
     }
 
     while (1) {
         size_t bytes_sent = 0;
-        while (bytes_sent < DATA_SIZE) {
-            size_t bytes_to_send = BUFFER_SIZE < DATA_SIZE - bytes_sent ? BUFFER_SIZE : DATA_SIZE - bytes_sent;
-            memcpy(buffer, data + bytes_sent, bytes_to_send);
-            ssize_t sent = send(sock, buffer, bytes_to_send, 0);
+        while (bytes_sent < DATA_SIZE) {            // while not all data sent
+            ssize_t sent = send(sock, data + bytes_sent, DATA_SIZE - bytes_sent, 0);   // send next chunk of data
             if (sent < 0) {
                 perror("ERROR writing to socket");
             }
-            bytes_sent += sent;
+            bytes_sent += sent;     // add to the total bytes sent
         }
         printf("File sent successfully.\n");
         
         char choice;
-        printf("Do you want to send the file again? (y/n): ");
+        printf("Do you want to send the file again? (y/n): ");    // send again?
         scanf(" %c", &choice);
         if (choice != 'y') {
             break;
         }
     }
 
-    send(sock, "exit", strlen("exit"), 0);
+    send(sock, "exit", strlen("exit"), 0);    // if said no then send exit
 
-    close(sock);
+    close(sock); // close connection
 
     return 0;
 }
